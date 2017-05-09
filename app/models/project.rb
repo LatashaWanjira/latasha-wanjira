bf56945cb0e7d3c777e9341455ca9fd1a4f654c6
LatashaWanjira <latasha.wanjira@gmail.com>
2017-05-09 19:10:25 +0300

    change of landing page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", large: "500x600>" }
+  has_attached_file :image, styles: { medium: "300x300>", large: "800x800>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :project_name, 	:presence => true
   validates :description, 	:presence => true
